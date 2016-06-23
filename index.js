78591953031e7ed25cbf273fc76212c182849d31
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-06-24 00:01:59 +0530

    updating

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
 var log = require('logger')('clustor');
 var cluster = require('cluster');
-var procevent = require('procevent');
+var os = require('os');
 
-var cpus = require('os').cpus().length;
-
-var end = function (starting, listening, address, done) {
-    if (starting > 0 || listening > 0) {
-        return;
-    }
-    log.debug('all workers started');
-    var master = procevent(process);
-    master.emit('started', address.port);
-    master.destroy();
-    log.debug(JSON.stringify(address));
-    done(false, address);
-};
-
-module.exports = function (run, done, forks) {
-    if (typeof done !== 'function') {
-        forks = done;
-        done = null;
-    }
+module.exports = function (run) {
     if (cluster.isWorker) {
         return run();
     }
-    forks = forks || cpus;
-    var address;
-    var listening = forks;
-    var starting = forks;
     var i;
-    for (i = 0; i < listening; i++) {
-        (function () {
-            var worker = cluster.fork();
-            var pevent = procevent(worker.process);
-            pevent.once('started', function () {
-                log.debug('worker started');
-                pevent.destroy();
-                end(--starting, listening, address, done);
-            });
-        }());
+    var cpus = os.cpus().length;
+    for (i = 0; i < cpus; i++) {
+        cluster.fork();
     }
     cluster.on('exit', function (worker, code, signal) {
-        log.debug('worker stopped | pid:%s, signal:%s, code:%s', worker.process.pid, signal, code);
-        //log.debug('%s worker restarting', domain);
-        //cluster.fork();
-    });
-    cluster.on('listening', function (worker, addrezz) {
-        log.debug('worker listening');
-        address = address || addrezz;
-        end(starting, --listening, address, done);
+        log.debug('worker stopped pid:%s, signal:%s, code:%s', worker.process.pid, signal, code);
+        cluster.fork();
     });
 };
